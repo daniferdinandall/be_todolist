cb8792b1a682e69136605bfef4ec75090672b92a
dani ferdinan <daniferdinandall@gmail.com>
2024-01-02 21:29:47 +0700

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/daniferdinandall/be_todolist
+
+go 1.21.3
+
+require go.mongodb.org/mongo-driver v1.13.1
---
+++ type.go
@@ -0,0 +1,34 @@
+package todolist
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Password    string             `json:"password" bson:"password"`
+	Email       string             `json:"email" bson:"email"`
+	Role        string             `json:"role,omitempty" bson:"role,omitempty"`
+	PhoneNumber string             `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
+}
+
+type Todolist struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	UserID      primitive.ObjectID `bson:"userid,omitempty" json:"userid,omitempty"`
+	Title       string             `bson:"title,omitempty" json:"title,omitempty"`
+	Description string             `bson:"description,omitempty" json:"description,omitempty"`
+	DueDate     string             `bson:"duedate,omitempty" json:"duedate,omitempty"`
+	Priority    int                `json:"priority,omitempty" bson:"priority,omitempty"`
+	Completed   bool               `json:"completed,omitempty" bson:"completed,omitempty"`
+}
+
+type Payload struct {
+	ID    primitive.ObjectID `json:"id"`
+	Email string             `json:"email"`
+	Exp   time.Time          `json:"exp"`
+	Iat   time.Time          `json:"iat"`
+	Nbf   time.Time          `json:"nbf"`
+}
